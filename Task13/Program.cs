ae1cb916d221754374f398e049c62f5f7e00dc26
AlinaKov <alkovkova@gmail.com>
2022-08-13 01:55:25 +0500

    Added Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число");
+        long number = Convert.ToInt64(Console.ReadLine());
+        if (number > 99)
+        {
+            Console.WriteLine("Третья цифра в числе - " + number.ToString()[2]);
+        }
+        else
+        {
+            Console.WriteLine("Третьей цифры нет");
+        }
+    }
+}
