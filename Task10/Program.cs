a4742611281f4e15bffda5cd622a12018a87b269
AlinaKov <alkovkova@gmail.com>
2022-08-13 01:29:15 +0500

    Added Task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+                Console.WriteLine("Введите трехзначное число");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        if (number1 > 99 & number1 < 1000)
+        {
+            while (number1 > 99)
+            {
+                number1 = number1 - 100;
+            }
+            int number = number1 / 10;
+            Console.WriteLine("Вторая цифра - " + number);
+        }
+        else
+        {
+            Console.WriteLine("Число не трехзначное");
+        }
+    }
+}
