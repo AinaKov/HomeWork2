b7f3538f5a0eac6a65462f39270fb167c1ef4ac0
AlinaKov <alkovkova@gmail.com>
2022-08-13 02:41:56 +0500

    added task15

---
+++ Task15/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Program
+{
+    static void WriteSelect(string[] namesArray)
+    {
+        for (int i = 0; i < namesArray.Length - 1; i++)
+        {
+            Console.Write(i + 1 + " - " + namesArray[i] + ", ");
+        }
+        Console.WriteLine(namesArray.Length + " - " + namesArray[namesArray.Length - 1] + ".");
+    }
+    static void SelectDay()
+    {
+        string[] namesDay = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+        WriteSelect(namesDay);
+        Console.WriteLine("Укажите день недели");
+        int indexDay = Convert.ToInt32(Console.ReadLine());
+        if (indexDay > 5)
+        {
+            Console.WriteLine(namesDay[indexDay - 1] + " - выходной");
+        }
+        else
+        {
+            Console.WriteLine(namesDay[indexDay - 1] + " - будний день");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        SelectDay();
+    }
+}
